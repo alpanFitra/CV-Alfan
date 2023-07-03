f2775a0f643bba0ddaebbbe9b145206a7ac13e6d
Alfan Fitra <holangiyesulga@gmail.com>
2023-07-04 06:28:52 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,226 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Cv Alfan</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: iPortfolio
+  * Updated: May 30 2023 with Bootstrap v5.3.0
+  * Template URL: https://bootstrapmade.com/iportfolio-bootstrap-portfolio-websites-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Mobile nav toggle button ======= -->
+  <i class="bi bi-list mobile-nav-toggle d-xl-none"></i>
+
+  <!-- ======= Header ======= -->
+  <header id="header">
+    <div class="d-flex flex-column">
+
+      <div class="profile">
+        <img src="assets/img/potoresmi.png" alt="" class="img-fluid rounded-circle">
+        <h1 class="text-light"><a href="index.php">Alfan Fitra Parhani</a></h1>
+        <div class="social-links mt-3 text-center">
+          <a href="https://api.whatsapp.com/send/?phone=628991360808&text&type=phone_number&app_absent=0" class="twitter"><i class="bx bxl-whatsapp"></i></a>
+          <a href="https://mail.google.com/mail/u/0/?view=cm&tf=1&fs=1&to=alpanfitra@gmail.com" class="facebook"><i class="bx bx-envelope"></i></a>
+          <a href="https://www.tiktok.com/@alpit47?_t8dgFf5zLpOU&_r=1" class="instagram"><i class="bx bxl-tiktok"></i></a>
+        </div>
+      </div>
+
+      <nav id="navbar" class="nav-menu navbar">
+        <ul>
+          <li><a href="#hero" class="nav-link scrollto active"><i class="bx bx-home"></i> <span>Beranda</span></a></li>
+          <li><a href="#about" class="nav-link scrollto"><i class="bx bx-user"></i> <span>Tentang Saya</span></a></li>
+          <li><a href="#resume" class="nav-link scrollto"><i class="bx bx-file-blank"></i> <span>Riwayat Pendidikan</span></a></li>
+          <li><a href="#services" class="nav-link scrollto"><i class="bx bx-server"></i> <span>Pengalaman Kerja</span></a></li>
+          <li><a href="#contact" class="nav-link scrollto"><i class="bx bx-envelope"></i> <span>Kontak</span></a></li>
+        </ul>
+      </nav><!-- .nav-menu -->
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex flex-column justify-content-center align-items-center" style="background-image: url(assets/img/bg-signup.jpg);">
+    <div class="hero-container" data-aos="fade-in">
+      <h1>Alfan Fitra Parhani</h1>
+    </div>
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+    <!-- ======= About Section ======= -->
+    <section id="about" class="about">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Tentang Saya</h2>
+          <p>Saya pernah melakukan Praktik Kerja Lapangan(PKL) sebagai pegawai IT diperusahan manufaktur. Saya juga berpengalaman membuat website dengan menggunakan framework CodeIgniter.</p>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-4" data-aos="fade-right">
+            <img src="assets/img/potoresmi.png" class="img-fluid" alt="">
+          </div>
+          <div class="col-lg-8 pt-4 pt-lg-0 content" data-aos="fade-left">
+            <h3>Profil</h3>
+            <div class="row">
+              <div class="col-lg-12">
+                <ul>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Tempat Tanggal Lahir :</strong> <span>Banjar, 19 November 2004</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Alamat:</strong> <span>Cikabuyutan Timur, Kota Banjar, Jawa Barat</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>No. Telepon:</strong> <span>08991360808</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Jenis Kelamin:</strong> <span>Laki-Laki</span></li>
+
+                  <li><i class="bi bi-chevron-right"></i> <strong>Agama:</strong> <span>Islam</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Email:</strong> <span>alpanfitra@gmail.com</span></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End About Section -->
+
+    <!-- ======= Resume Section ======= -->
+    <section id="resume" class="resume">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Riwayat Pendidikan</h2>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-6" data-aos="fade-up">
+            <div class="resume-item">
+              <h4>SDN 2 Hegarsari</h4>
+              <h5>2011 - 2017</h5>
+            </div>
+            <div class="resume-item">
+              <h4>SMPN 1 Banjar</h4>
+              <h5>2017 - 2020</h5>
+            </div>
+            <div class="resume-item">
+              <h4>SMKN 1 Banjar</h4>
+              <h5>2020 - 2023</h5>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Resume Section -->
+
+    <!-- ======= Services Section ======= -->
+    <section id="services" class="services">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Pengalaman Kerja</h2>
+          <p>Saya pernah Praktik Kerja Lapangan(PKL) di perusahaan PT. PIndad Bandung, sebagai pegawai yang bekerja di bagian divisi IT.</p>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up">
+            <div class="icon"><i class="bi bi-briefcase"></i></div>
+            <h4 class="title"><a href="">Membantu pegawai lain membuat website menggunakan CodeIgniter</a></h4>
+          </div>
+          <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up" data-aos-delay="100">
+            <div class="icon"><i class="bi bi-card-checklist"></i></div>
+            <h4 class="title"><a href="">Mendesain ulang website yang berkaitan dengan divisi IT</a></h4>
+          </div>
+          <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up" data-aos-delay="200">
+            <div class="icon"><i class="bi bi-bar-chart"></i></div>
+            <h4 class="title"><a href="">Membantu pekerjaan pegawai di divisi IT</a></h4>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End Services Section -->
+
+    <!-- ======= Contact Section ======= -->
+    <section id="contact" class="contact">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Kontak</h2>
+        </div>
+
+        <div class="row" data-aos="fade-in">
+
+          <div class="col-lg-12 d-flex align-items-stretch">
+            <div class="info">
+              <div class="address">
+                <i class="bi bi-geo-alt"></i>
+                <h4>Alamat:</h4>
+                <p>Cikabuyutan Timur RT.04/11, Kel. Hegasrsari, Kec. Pataruman, Kota Banjar, Jawa Barat</p>
+              </div>
+
+              <div class="email">
+                <i class="bi bi-envelope"></i>
+                <h4>Email:</h4>
+                <p>alpanfitra@gmail.com</p>
+              </div>
+
+              <div class="phone">
+                <i class="bi bi-phone"></i>
+                <h4>No. Telepon:</h4>
+                <p>08991360808</p>
+              </div>
+              <iframe src="https://www.google.com/maps/embed?pb=!4v1688374519757!6m8!1m7!1sc8AZBqHlzIHuh5Ksn_E4_Q!2m2!1d-7.377434996784271!2d108.5445932366928!3f133.912!4f0!5f0.7820865974627469" frameborder="0" style="border:0; width: 100%; height: 290px;" allowfullscreen></iframe>
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Contact Section -->
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/typed.js/typed.umd.js"></script>
+  <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
